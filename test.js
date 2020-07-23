f37894ecff9cc5fd3f0e32a39482d154bd5da423
Apple <apple@appurunoMacBook-puro.local>
2020-07-23 16:46:14 +0900

    json version

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ const f = fetch('http://127.0.0.1:8000/', {
 f.then(res=> {
     console.log(res)
     console.log(res.text())
-    return res.text()
+    return res.json()
 })
 .then((data) => {
     console.log(data)
